5c20d873d3a00c51ed7d1fc81ccca71e8c8130fe
meghanapuli <49053165+meghanapuli@users.noreply.github.com>
2024-09-17 17:31:25 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -7,12 +7,33 @@ public:
   TestDataPair(string _input,int _expectedValue) : input{_input}, expectedValue{_expectedValue}{
   }
 };
+class StringCalculatorDataDrivenFixture:public testing::Test{
+protected:
+  vector<TestDataPair> datalist;
+//before each testcase
+void SetUp(){
+  dataList.pushback(new TestDataPair {",0});
+  dataList.pushback(new TestDataPair {"0",0});
+  dataList.pushback(new TestDataPair {1,1});
+  dataList.pushback(new TestDataPair {"1,2",3});
+  dataList.pushback(new TestDataPair {"1,2,3",6});
+}
+// after each test case
+void TearDown(){
+};
+TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
+  for(TestDataPair* DataPairPtr :dataList){
+   int actualValue=Add(dataPairPtr->input);
+        ASSERT_EQ(actualValue,dataPairPtr->expectedValue);
+    }
+}
+
 
 TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
   vector<TestDataPair>dataList;
   TestDataPair pair_one {"",0};
   TestDataPair pair_two {"0",0};
-  TestDataPair pair_three{"",0};
+  TestDataPair pair_three {1,1};
   TestDataPair pair_four {"1,2",3};
   TestDataPair pair_five {"1,2,3",6};
   dataList.push_back(pair_one);
