943206053202d287ab07083aeaa4b83808bed40e
meghanapuli <49053165+meghanapuli@users.noreply.github.com>
2024-09-18 09:49:56 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -36,15 +36,15 @@ TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
   // vector<TestDataPair>datalist;
   TestDataPair pair_one {"",0};
   TestDataPair pair_two {"0",0};
-  TestDataPair pair_three {1,1};
+  TestDataPair pair_three {"1",1};
   TestDataPair pair_four {"1,2",3};
   TestDataPair pair_five {"1,2,3",6};
   
-  dataList.push_back(pair_one);
-  dataList.push_back(pair_two);
-  dataList.push_back(pair_three);
-  dataList.push_back(pair_four);
-  dataList.push_back(pair_five);
+  datalist.push_back(pair_one);
+  datalist.push_back(pair_two);
+  datalist.push_back(pair_three);
+  datalist.push_back(pair_four);
+  datalist.push_back(pair_five);
   // Iterate using a range-based for loop
     for(TestDataPair DataPair : datalist){
         int actualValue=Add(DataPair.input);
