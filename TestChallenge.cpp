8457cc5dcfa1ab044ad41e0c4a0b21cf224a2920
meghanapuli <49053165+meghanapuli@users.noreply.github.com>
2024-09-18 17:36:26 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -10,7 +10,7 @@ return alertFailureCount;
 
 void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlerterPtr->alert(Celcius);
+    int returnCode = networkAlerterPtr->alert(celcius);
     if (returnCode != 200) {
         alertFailureCount += 1;
     }
